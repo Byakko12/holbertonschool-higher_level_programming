5f460b874b956b85e08176838966bd14db858432
David <davidlandg@gmail.com>
2022-01-01 16:23:21 -0500

    [FIX] - string

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         print("{:d} argument:".format(len(sys.argv) - 1))
         print("{:d}: {}".format(len(sys.argv) - 1, sys.argv[1]))
     else:
-        print("{:d} argument:".format(len(sys.argv) - 1))
+        print("{:d} arguments:".format(len(sys.argv) - 1))
         for i in sys.argv:
             if counter >= 1:
                 print("{:d}: {}".format(counter, i))
