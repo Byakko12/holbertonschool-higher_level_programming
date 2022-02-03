1c64961d610ffd13aa7c5fe7d8c0ccf7763e4028
David <davidlandg@gmail.com>
2022-02-02 22:27:01 -0500

    [ADD] - pascal_triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """pascal_triangle"""
+    if n <= 0:
+        return ""
+
+    triangle = [[1]]
+    for cur_row in range(1, n):
+        row = [1]
+        prev_row = triangle[cur_row - 1]
+        for elem in range(1, cur_row):
+            row.append(prev_row[elem] + prev_row[elem - 1])
+        row.append(1)
+        triangle.append(row)
+    return (triangle)
