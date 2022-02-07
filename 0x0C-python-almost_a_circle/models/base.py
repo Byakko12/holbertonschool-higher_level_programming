09ede1ac4da583ab1a1b3b58ab68600d0f1b8715
David <davidlandg@gmail.com>
2022-02-07 12:52:03 -0500

    [ADD] - rectangle base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/ur/bin/python3
+"""class Base"""
+
+
+class Base:
+    __nb_objects = 0
+    """contructor of class"""
+
+    def __init__(self, id=None):
+        self.id = id
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
