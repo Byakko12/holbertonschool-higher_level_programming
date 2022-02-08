3cd2016b8ee39b450446d3e91af60bc52a41bea7
David <davidlandg@gmail.com>
2022-02-07 19:17:25 -0500

    [fix] - conditional error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,7 +26,7 @@ class Rectangle (Base):
         """widht setter"""
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
         else:
             self.__width = value
@@ -41,7 +41,7 @@ class Rectangle (Base):
         """height setter"""
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
         else:
             self.__height = value
