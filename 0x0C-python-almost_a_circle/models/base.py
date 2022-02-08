c66f9efa7974084967649c898d33f574005fbd49
David <davidlandg@gmail.com>
2022-02-07 20:40:31 -0500

    [UPDATE] - base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """class Base"""
 
 
+import json
+
 class Base:
     """contructor of class"""
     __nb_objects = 0
@@ -13,3 +15,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """to json string"""
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
