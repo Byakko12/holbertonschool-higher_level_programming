e17aebbe0da01abfeb12c939d2d704e87de24fe5
Byakko12 <davidlandg@gmail.com>
2022-03-17 18:03:19 -0500

    [ADD] - 13-count_shows_by_genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
+    LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY
+    tv_shows.title,
+    tv_show_genres.genre_id;
