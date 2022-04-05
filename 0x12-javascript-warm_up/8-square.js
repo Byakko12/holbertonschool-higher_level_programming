d9e4e5114c6ebfa970460072946144c272650696
Byakko12 <davidlandg@gmail.com>
2022-04-05 12:52:31 -0500

    [ADD] - 8-square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const number = parseInt(argv[2]);
+if (!number){
+  console.log('Missing size')
+}
+for (let i = 0; i < number; i++) {
+  console.log('X'.repeat(number));
+}
