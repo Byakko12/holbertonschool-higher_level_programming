413fdc28a4cc924b34d79d09bc7a067c43ef5b6a
Byakko12 <davidlandg@gmail.com>
2022-04-05 12:06:06 -0500

    [ADD] - 7-multi_c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const number = parseInt(argv[2]);
+if (!number) {
+  console.log('Missing number of occurrences');
+}
+for (let i = 0; i < number; i++) {
+  console.log('C is fun');
+}
