642106ccb54e6e4c20b91f78b0cc4a0928ae6d35
Byakko12 <davidlandg@gmail.com>
2022-04-07 18:22:41 -0500

    [FIX] - identation

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-const Rectangle = require('./5-square');
-
-class Square extends Rectangle {
+const Squares = require('./5-square');
+class Square extends Squares {
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
   }
 }
-
 module.exports = Square;
