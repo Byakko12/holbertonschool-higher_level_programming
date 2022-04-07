d69bd26d83af32c85da965ac4f6a3e36331ae82a
Byakko12 <davidlandg@gmail.com>
2022-04-07 18:30:34 -0500

    [FIX] - require and extends

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-const Squares = require('./5-square');
-class Square extends Squares {
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
