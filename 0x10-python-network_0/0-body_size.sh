2d63871423bbc34391902d3614f7d69a82b75320
Byakko12 <davidlandg@gmail.com>
2022-05-18 20:06:40 -0500

    [FIX] - File 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #that takes in a URL, sends a request to that URL, and display
-$ curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
+$ curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
