5ffa9dba5a50f1afa10b404d305dd7fc7625e218
Byakko12 <davidlandg@gmail.com>
2022-05-19 00:46:07 -0500

    fix

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL as an argument, sends a GET request to the URL, and displays the body
-curl -s -X GET "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -s -X GET "$1" -H "X-School-User-Id: 98"
